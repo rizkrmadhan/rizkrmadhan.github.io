1dfc4ba75b91e0e88d2f024e1a7cc6dd6e6c3c79
rizkrmadhan <147575067+rizkrmadhan@users.noreply.github.com>
2024-01-11 19:29:56 +0700

    Add files via upload

---
+++ UAS_RizkiRamadhan/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Rizki Ramadhan</title>
+	<link rel="stylesheet" type="text/css" href="css.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head>
+<body>
+	<!-- loader -->
+	<div class="bg-loader">
+		<div class="loader"></div>
+	</div>
+
+	<!-- header -->
+	<div class="medsos">
+		<div class="container">
+		</div>
+	</div>
+	<header>
+		<div class="container">
+			<h1><a href="index.php">RIZKI RAMADHAN</a></h1>
+			<ul>
+				<li class="active"><a href="index.php">HOME</a></li>
+				<li><a href="about.php">PENDIDIKAN</a></li>
+				<li><a href="service.php">HOBI</a></li>
+				<li><a href="contact.php">CONTACT</a></li>
+			</ul>
+		</div>
+	</header>
+
+	<!-- banner -->
+	<section class="banner">
+		<h2>WELCOME TO MY WEBSITE</h2>
+	</section>
+
+	<!-- about -->
+	<section class="about">
+		<div class="container">
+			<h3>TENTANG SAYA</h3>
+			<p>Halo, perkenalkan, saya <strong>Rizki Ramadhan</strong>. Saya lahir di <strong>Magetan</strong>.  pada tanggal <strong>30 November 2002</strong>. Saat ini, saya tinggal di <strong> Desa Prampelan, Kecamatan Karangrejo, Kabupaten Magetan</strong>, dan sedang menempuh pendidikan di <strong> Universitas PGRI MADIUN jurusan Teknik Informatika</strong>. Selain kegiatan akademis, saya juga memiliki hobi seperti Bermain Sepak Bola, Berkendara, Berenang dan jangan melupakan kewajiban kita sebagai umat islam yaitu dengan beribadah. Kegiatan kegiatan ini membantu saya menjaga keseimbangan antara pekerjaan dan kehidupan pribadi.</p>
+		</div>
+	</section>
+
+	<!-- service -->
+	<section class="service">
+		<div class="container">
+			<h3>HOBI</h3>
+			<div class="box">
+				<div class="col-4">
+					<div class="icon"><i class="far fa-futbol"></i></div>
+					<h4>SEPAK BOLA</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-motorcycle"></i></i></div>
+					<h4>BERKENDARA</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-swimmer"></i></i></div>
+					<h4>BERENANG</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-pray"></i></div>
+					<h4>BERIBADAH</h4>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- footer -->
+	<footer>
+		<div class="container">
+			<small>Copyright &copy; 2023 - RizkiRamadhan. All Rights Reserved.</small>
+		</div>
+	</footer>
+
+
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$(".bg-loader").hide();
+		})
+	</script>
+</body>
+</html>
